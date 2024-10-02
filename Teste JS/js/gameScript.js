8664d3a2d21b993de4f49af765de4e0c1b6184d7
Andre Vitor <andrevbm.288@gmail.com>
2024-10-02 11:33:47 -0300

    Novo sistema de colisão e comentários melhores

--- Teste JS/js/gameScript.js
+++ Teste JS/js/gameScript.js
@@ -2,19 +2,46 @@
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); 
 
-    for (let card of cards) {
-        drawCard(card);
-    }
-    
+    // Desenha todos os slots
     for (let slot of slots) {
         drawSlot(slot);
     }
 
+    // Desenha todas as cartas
+    for (let card of cards) {
+        drawCard(card);
+    }
+
+    // Espera o próximo frame para repetir o processo
     requestAnimationFrame(update);
 }
+
+// Checagem de colisão
+function isColliding(x, y, obj) {
+    // Caso (x, y, obj2) - Colisão de ponto com objeto
+    if (arguments.length == 3) {
+        return x > obj.x && x < obj.x + obj.width &&
+               y > obj.y && y < obj.y + obj.height;
+               
+    // Caso (obj1, obj2) - Colisão de dois objetos
+    } else if (arguments.length == 2) {
+        let obj1 = x;
+        let obj2 = y;
+
+        return obj1.x < obj2.x + obj2.width &&
+               obj1.x + obj1.width > obj2.x &&
+               obj1.y < obj2.y + obj2.height &&
+               obj1.y + obj1.height > obj2.y;
+    }
+
+    // Sem argumentos suficientes
+    return false;
+}
+
 
 // Permite que o canvas receba foco e possa capturar eventos de teclado
 canvas.tabIndex = 1000; 
 canvas.focus();
 
-update();  // Inicializa o loop
+// Inicializa o loop
+update();
--- Teste JS/js/slot.js
+++ Teste JS/js/slot.js
@@ -15,5 +15,33 @@ function createSlot(x, y) {
 
 // Desenhar slot
 function drawSlot(slot) {
-    
+    ctx.save();  
+
+    ctx.translate(slot.x, slot.y);
+
+    ctx.fillStyle = "rgb(0, 0, 0, 0.5)";
+    ctx.fillRect(0, 2, slot.width, slot.height);
+
+    ctx.fillStyle = "rgb(246, 227, 148)";
+    ctx.fillRect(0, 0, slot.width, slot.height);
+
+    ctx.restore();
 }
+
+// EventListener para soltar o mouse
+canvas.addEventListener('mouseup', () => {
+    if (isDragging && draggableCard) {
+        if (isColliding(draggableCard.x, draggableCard))
+        draggableCard.drag = false;
+        isDragging = false;
+        draggableCard.rot = Math.random() * 12 - 6;
+        draggableCard = null;
+    }
+});
+
+// Cria um slot no centro
+const newSlot = createSlot(
+    (document.getElementById("gameCanvas").width / 2) - (slotWidth / 2),    // Centro horizontal da tela
+    (document.getElementById("gameCanvas").height / 2) - (slotHeight / 2)   // Centro vertical da tela
+)
+slots.push(newSlot);
